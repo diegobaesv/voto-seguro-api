28633d8b2cedc7d452df3ffefa1fa3bf71e5c1c8
Diego Baes <diegobaesv@gmail.com>
2025-11-24 22:24:57 -0500

    mejorado con baseresponse

--- src/controllers/persona.controller.ts
+++ src/controllers/persona.controller.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from "express";
 import * as personaService from "../services/persona.service";
+import { BaseResponse } from "../shared/base-response";
 
 export const verificarPersona = async (req: Request, res: Response) => {
     try {
         const data = req.body;
         const persona = await personaService.verificarPersona(data);
-        res.json(persona);
+        if(!persona){
+            res.status(404).json(BaseResponse.error('No se encontraron datos'));
+            return;
+        }
+        res.json(BaseResponse.success(persona));
     } catch (error) {
         console.error('verificarPersona:error',error);
-        res.status(500).json({ error: error.message });
+        res.status(500).json(BaseResponse.error(error.message));
     }
 }
